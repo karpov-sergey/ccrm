5c581634b311aaf1d2dd2558919712152769e74c
Sergey Karpov <s.i.karpoff@gmail.com>
2025-09-11 19:34:45 +0200

    add associated contact to task card

--- src/components/board/TaskCard.vue
+++ src/components/board/TaskCard.vue
@@ -3,9 +3,11 @@ import { computed } from 'vue';
 import dayjs from 'dayjs';
 import DOMPurify from 'dompurify';
 
-import { Badge } from '@/components/ui/badge';
-
 import { useDueDateVariant } from '@/composables/dateStatus.ts';
+import { useNameAbbreviation } from '@/composables/common.ts';
+
+import { Badge } from '@/components/ui/badge';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 
 import { TimerReset, CheckCheck } from 'lucide-vue-next';
 
@@ -16,6 +18,7 @@ const props = defineProps<{
 }>();
 
 const { dueDateBadgeVariant } = useDueDateVariant();
+const { nameAbbreviation } = useNameAbbreviation();
 
 const taskChecklistProgress = computed(() => {
 	const total = props.task.checklist?.length;
@@ -41,14 +44,30 @@ const isAdditionalInfoVisible = computed(() => {
 		props.task.date
 	);
 });
+
+const contactName = computed(() => {
+	if (!props.task.associated_contact) {
+		return '';
+	}
+
+	return `${props.task.associated_contact?.first_name} ${props.task.associated_contact?.last_name}`;
+});
 </script>
 
 <template>
 	<div class="p-2 text-sm rounded-lg border border-transparent bg-white shadow">
-		<div
-			class="mb-2 font-semibold font-sans tracking-wide text-sm last-of-type:mb-0"
-		>
-			{{ task.title }}
+		<div class="flex items-center justify-between mb-2 last-of-type:mb-0">
+			<div
+				class="mb-2 font-semibold font-sans tracking-wide text-sm last-of-type:mb-0"
+			>
+				{{ task.title }}
+			</div>
+
+			<Avatar>
+				<AvatarFallback>
+					{{ nameAbbreviation(contactName) }}
+				</AvatarFallback>
+			</Avatar>
 		</div>
 		<div
 			v-if="purifiedDescription.length"
@@ -56,28 +75,30 @@ const isAdditionalInfoVisible = computed(() => {
 		>
 			{{ purifiedDescription }}
 		</div>
-		<div v-if="isAdditionalInfoVisible" class="flex gap-2 items-center">
-			<Badge
-				v-if="task.checklist?.length"
-				variant="outline"
-				class="flex gap-1 items-center text-xs"
-			>
-				<CheckCheck
-					class="size-4"
-					:class="{
-						'text-primary': isAllTaskCheckboxItemsChecked,
-					}"
-				/>
-				{{ taskChecklistProgress.done }} / {{ taskChecklistProgress.total }}
-			</Badge>
-			<Badge
-				v-if="task.date"
-				:variant="dueDateBadgeVariant(task.date)"
-				class="flex gap-1 text-xs"
-			>
-				<TimerReset class="size-4" />
-				{{ dayjs(task.date).format('MMM DD') }}
-			</Badge>
+		<div v-if="isAdditionalInfoVisible" class="flex gap-2 justify-between">
+			<div class="flex gap-2 items-center">
+				<Badge
+					v-if="task.checklist?.length"
+					variant="outline"
+					class="flex gap-1 items-center text-xs"
+				>
+					<CheckCheck
+						class="size-4"
+						:class="{
+							'text-primary': isAllTaskCheckboxItemsChecked,
+						}"
+					/>
+					{{ taskChecklistProgress.done }} / {{ taskChecklistProgress.total }}
+				</Badge>
+				<Badge
+					v-if="task.date"
+					:variant="dueDateBadgeVariant(task.date)"
+					class="flex gap-1 text-xs"
+				>
+					<TimerReset class="size-4" />
+					{{ dayjs(task.date).format('MMM DD') }}
+				</Badge>
+			</div>
 		</div>
 	</div>
 </template>
